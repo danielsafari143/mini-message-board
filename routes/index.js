8740c709c3b3bcc5e7b23ae3010d9b76ff647939
danielsafari143 <danielsafari143@gmail.com>
2023-06-17 17:19:04 +0200

    add data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const message = [
+  {
+    user : "Danie safari",
+    text : "Hi there",
+    date : Date()
+  },
+  {
+    user : "Rodrigue safari",
+    text : "Hi there",
+    date : Date() 
+  },
+  {
+    user : "Kerene safari",
+    text : "Hi there",
+    date : Date() 
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {data : message});
 });
 
 module.exports = router;
