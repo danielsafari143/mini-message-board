2b566927ec5c704114c0ed69a42b46f62d36ec5c
danielsafari143 <danielsafari143@gmail.com>
2023-06-17 18:54:03 +0200

    ADD LINK AND POST

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,13 @@ router.get('/', function(req, res, next) {
   res.render('index', {data : message});
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', {data : message});
+});
+
+router.post('/new', function(req, res, next) {
+  message.push({user : req.body.user , text: req.body.text , date : Date()})
+  res.redirect("/");
+});
+
 module.exports = router;
